9f391af774d80dfa022538744b19867f064660d7
Pete Stewart <petestew@gmail.com>
2020-08-08 10:24:43 -0500

    create initial setup branch
     Please enter the commit message for your changes. Lines starting

---
+++ src/App/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>INSIDE APP COMPONENT</h2>
+        <button className="btn btn-info">
+          <i className="fab fa-react"></i> I am a button
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
